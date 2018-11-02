86b81fa63f193a6051e01735830395d1c99b95e2
Chukwualuka Chiama <lukaschiama@gmail.com>
2018-11-02 23:18:59 +0100

    Initialize server for trello clone

---
+++ server/index.js
@@ -0,0 +1,106 @@
+const { GraphQLServer } = require('graphql-yoga');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/test2", { useNewUrlParser: true });
+
+const List = mongoose.model("List", {
+  name: String,
+});
+
+const Card = mongoose.model("Card", {
+  title: String,
+  description: String,
+  comment: String,
+  list: String,
+});
+
+const typeDefs = `
+  type Query {
+    lists: [List]
+    cards: [Card]
+  }
+
+  type List {
+    id: ID
+    name: String
+    card: [Card]
+  }
+
+  type Card {
+    id: ID
+    listId: String
+    title: String
+    description: String
+    comment: String
+    list: List
+  }
+
+  type Mutation {
+    createList(name: String): List!
+    updateList(id: ID name: String): List!
+    deleteList(id: ID): Boolean!
+
+    createCard(title: String, listId: String description: String, comment: String): Card!
+    updateCard(id: ID, listId: String title: String, description: String, comment: String): Card!
+    deleteCard(id: ID): Boolean!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    lists: () => List.find(),
+    cards: () => Card.find()
+  },
+
+  List: {
+    card: async (_, {id}) => {
+     return (await Card.find({_id: id}));
+    }
+  },
+
+  Card: {
+    list: async (_, {listId}) => {
+      return (await List.findOne({_id: listId}));
+    }
+  },
+
+  Mutation: {
+    createList: async (_, { name }) => {
+      const list = new List({ name });
+      await list.save();
+      return list;
+    },
+    updateList: async (_, { id, name }) => {
+      const list = List.findOne({_id: id})
+      await List.findOneAndUpdate( {_id: id}, {name} );
+      return list;
+    },
+    deleteList: async (_, { id}) => {
+      await List.findOneAndDelete({_id: id});
+      return true;
+    },
+
+    createCard: async (_, { title, description, comment, listId }) => {
+      const card = new Card({ title, description, comment , listId });
+      await card.save();
+      await List.findOneAndUpdate({_id: listId});
+      return card;
+    },
+    updateCard: async (_, { id, title, description, comment, listId }) => {
+      await Card.findOneAndUpdate({_id: id}, { title, description, comment, listId });
+      await List.findOneAndUpdate({_id: listId});
+      const card = Card.findOne({_id: id});
+      return card;
+    },
+    deleteCard: async (_, { id }) => {
+      await Card.findOneAndDelete({_id: id});
+      return true;
+    }
+  }
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+mongoose.connection.once("open", function() {
+  server.start()
+} 
+)
