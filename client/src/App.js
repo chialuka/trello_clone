6953cd2ea3b435d6ef24c2d7ca7afbbb21648c37
Chukwualuka Chiama <lukaschiama@gmail.com>
2018-11-04 23:56:51 +0100

    add modal to cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gql from "graphql-tag";
 import { graphql, compose } from 'react-apollo';
+import { Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Button } from 'reactstrap';
 
 const ListsQuery = gql`
 {
@@ -83,6 +84,15 @@ mutation($id: ID!){
 `;
 
 class App extends Component {
+  state = {
+    modal: false,
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
 
   render() {
     console.log(this.props.card, this.props.list)
@@ -92,12 +102,44 @@ class App extends Component {
     return (
       <div className="page">{lists.map(list =>
         <div key={list.id} className="list">
+          {list.name}
           {list.card.map(card =>
-            <div key={card.id}>
-              {list.name}
-                <li>{card.title}</li>
-                <li>{card.comment}</li>
-                <li>{card.description}</li>
+            <div key={card.id} className='item'>
+              <p className="title" onClick={this.toggle}>{card.title}</p>
+              <Modal 
+              isOpen={this.state.modal} 
+              toggle={this.toggle}
+              >
+                <ModalHeader toggle={this.toggle}>{card.title}
+                  <p> in List: {list.name}</p>
+                </ModalHeader>
+                <ModalBody>
+                  <div className="description">Description: 
+                    <li>{card.description}</li>
+                    <Form>
+                      <FormGroup>
+                        <Input
+                          type="text"
+                          name="text"
+                          placeholder="Add more description" />
+                      </FormGroup>
+                      <Button size="sm">Save</Button>
+                    </Form>
+                  </div>
+                  <div className="comment">Comments: 
+                    <li>{card.comment}</li>
+                    <Form>
+                      <FormGroup>
+                        <Input
+                          type="text"
+                          name="text"
+                          placeholder="Add more comments" />
+                      </FormGroup>
+                      <Button size="sm">Save</Button>
+                    </Form>
+                  </div>
+                </ModalBody>
+              </Modal>
             </div>)}
         </div>)}
       </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import * as serviceWorker from './serviceWorker';
-import App from './App.js'
+import App from './App.js';
 
 
 const client = new ApolloClient({
